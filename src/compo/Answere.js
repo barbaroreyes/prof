9448d715428639a5717f990e9d795505fba82b7f
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-04-06 21:52:58 -0500

    update

--- src/compo/Answere.js
+++ src/compo/Answere.js
@@ -1,19 +1,21 @@
 import React ,{useContext}from 'react';
 import {QuestionsContext} from '../context/questions'
-import {useParams} from 'react-router-dom'
+import {useParams ,useNavigate} from 'react-router-dom'
 const Answere = () => {
     const  {id} = useParams() 
+    const history = useNavigate()
     const questions  = useContext(QuestionsContext)
     const finder = questions.find( question => question.id === id)
     const {imageAnswere , answere} = finder
     console.log(finder , 'finder')
   return (
-    <div className="question ans">
+    <div className="question ans" >
      
      <div className="imageContainer">
      <img className="imageA" src={imageAnswere} alt=""/>
      </div>
      <p className="des">{answere}</p>
+     <button className="btn" onClick ={()=> history('/')}>Back</button>
        
     </div>
   );
--- src/compo/Question.js
+++ src/compo/Question.js
@@ -1,15 +1,15 @@
-// import React ,{useContext}from 'react';
 
 const Question = (props) => {
-    console.log(props.answere)
+  
   return (
     <div className="question ">
        
       <h3>{props.description}</h3>
       <div>
       <img  className="image" src ={props.image} alt ='' />
-      </div>
       
+      </div>
+     
     </div>
   );
 }
